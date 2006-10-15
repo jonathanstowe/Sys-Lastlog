1cffd0a07c3295b9c024bd68552e3ca61518ea2e
jonathan <jonathan@d4a3e428-c23c-0410-a93a-b1ce904c0fb3>
2006-10-15 18:33:14 +0000

    Added strict and warnings to test
    git-svn-id: http://jstowe.googlecode.com/svn/trunk/Sys-Lastlog@94 d4a3e428-c23c-0410-a93a-b1ce904c0fb3

--- test.pl
+++ test.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/perl -w
+use strict;
 use Test::More tests => 5;
 use_ok('Sys::Lastlog');
 my $ll;
@@ -8,4 +10,4 @@ my $llent;
 
 ok($llent = $ll->getlluid($>),"Get Entry by UID");
 ok($llent = $ll->getllnam((getpwuid($>))[0]),"Get Entry by logname");
-ok($t = $llent->ll_line(),"Get ll_line");
+ok(my $t = $llent->ll_line(),"Get ll_line");
